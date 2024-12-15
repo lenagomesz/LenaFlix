1b9c927474fcd9b10d805d7ecfcc79b16cef25aa
Helena Gomes <87838594+lenagomesz@users.noreply.github.com>
2024-12-15 20:43:15 +0000

    variaveis

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { FaFilter } from "react-icons/fa";
 
 const Filter = ({ onFilter }) => {
   const [isOpen, setIsOpen] = useState(false);
